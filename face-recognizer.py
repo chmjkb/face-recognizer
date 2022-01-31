28e2f4e0050a804845cb960b185e7d337491b1bb
Chmura <jakub.chmura@aptiv.com>
2022-01-31 10:41:56 +0100

    iterating over images

--- face-recognizer.py
+++ face-recognizer.py
@@ -16,21 +16,43 @@ def captureVid():
         
     vid.release()
     cv.destroyAllWindows()
-    
-people = [ f.path.lstrip('\\Faces') for f in os.scandir('Faces') if f.is_dir() ]
-faces_dir = os.getcwd() + '\\Faces'
 
 
-def trainModel(DIR, people):
-    for person in people:
-        path = os.path.join(DIR, person)
+people = [f.path.lstrip('\\faces') for f in os.scandir('faces') if f.is_dir()]
+faces_dir = os.getcwd() + '\\faces'
+haar_cascade = cv.CascadeClassifier('haar_cascades.xml')
+
+
+def create_faces(directory, people_list):
+    features = []
+    labels = []
+
+    for person in people_list:
+        path = os.path.join(directory, person)
         label = people.index(person)
 
         for img in os.listdir(path):
             img_path = os.path.join(path, img)
+            img_array = cv.imread(img_path)
+            gray = cv.cvtColor(img_array, cv.COLOR_BGR2RGB)
 
-    
-    
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+            for (x, y, w, h) in faces_rect:
+                faces_roi = gray[y:y+h, x:x+w]
+                features.append(faces_roi)
+                labels.append(label)
 
 
+def train_model(features, labels):
+    face_recognizer = cv.face.LBPHFaceRecognizer_create()
+    features = np.array(features, dtype='object')
+    labels = np.array(labels)
 
+    np.save('features.npy', features)
+    np.save('labels.npy', labels)
+
+    # Train the recognizer on the features and labels list
+    face_recognizer.train(features, labels)
+
+
+create_faces(faces_dir, people)
