a42d47ea6edcef7d38058d275d6f5ffae3bfd596
chmjkb <jakubchmura1607@gmail.com>
2022-02-01 10:06:52 +0100

    Informing whether the face is not recognized

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from cv2 import calcBackProject
 import numpy as np
 import cv2 as cv
+import os
 
 
 haar_cascade = cv.CascadeClassifier('haar_cascades.xml')
@@ -9,9 +9,14 @@ labels = np.load('labels.npy', allow_pickle=True)
 
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('faces_trained.yml')
-people = ['Rami Malek', 'Asap Rocky']
 
-def captureVid():
+people = []
+DIR = os.path.join(os.getcwd(), 'faces')  # Faces dir location
+for person in os.listdir(DIR):  # Iterating through faces dir to get people
+    people.append(person)
+
+
+def capture_vid():
     """Function responsible for capturing the webcam"""
 
     vid = cv.VideoCapture(0)
@@ -19,23 +24,25 @@ def captureVid():
     while True:  # Reads the video frame by frame
 
         ret, frame = vid.read()
-        
 
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        faces_rect = haar_cascade.detectMultiScale(gray_frame, 1.1, 4)
+        faces_rect = haar_cascade.detectMultiScale(gray_frame, 1.1, 4)  # Detecting a face
 
         for (x, y, w, h) in faces_rect:
             faces_roi = gray_frame[y:y+h, x:x+h]
-            label, confidence = face_recognizer.predict(faces_roi)
+            label, confidence = face_recognizer.predict(faces_roi)  # Recognizing a face
             print(f"Label = {label}, confidence = {confidence}")
             
             if confidence > 99:
                 cv.putText(frame, str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), 2)
                 cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+            else:
+                cv.putText(frame, 'unknown', (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), 2)
+                cv.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
             
         cv.imshow('frame', frame)
 
         if cv.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
             break
 
-captureVid()
+capture_vid()
